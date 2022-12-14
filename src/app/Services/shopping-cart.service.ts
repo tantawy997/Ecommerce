5e3db89eb258e92295b6165557b616f3f8ac5f5a
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2022-12-14 13:52:23 +0200

    add cart service

---
+++ src/app/Services/shopping-cart.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+storage = window.localStorage;
+  constructor(public http:HttpClient) { }
+
+  GetShoppingCart(){
+    // const getProduct = this.storage.getItem('products');
+    // return getProduct ? JSON.parse(getProduct) : [];
+  }
+
+  ClearCart(){
+    this.storage.clear();
+  }
+}
