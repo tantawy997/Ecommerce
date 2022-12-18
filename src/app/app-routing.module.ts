69041ac24f0935851d12863ee893abf9842500eb
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2022-12-18 10:44:39 +0200

    debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 import { ProductDetailsComponent } from './products/product-details/product-details.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ShoppingCartComponent } from './products/shopping-cart/shopping-cart.component';
@@ -11,7 +12,7 @@ const routes: Routes =
   {path: 'products', component:ProductListComponent},
   {path:'products/details/:id',component:ProductDetailsComponent},
   {path:'products/cart', component:ShoppingCartComponent},
-  {path: 'login', component:LoginComponent},
+  {path: 'login', component:UserLoginComponent},
   {path:'',redirectTo: "/",pathMatch:"full"},
   {path:'**', component:ProductListComponent}
 ];
