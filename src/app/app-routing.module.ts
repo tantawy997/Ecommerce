daea0bf5bd9752f59f07d67bb0145398a7e9cf42
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-01-01 00:25:46 +0200

    fix rounting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './products/product-details/product-details.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ShoppingCartComponent } from './products/shopping-cart/shopping-cart.component';
+import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes =
   [
   {path:"Products",component:ProductListComponent},
   {path:"Products/details/:id", component:ProductDetailsComponent},
   {path:"Products/cart", component:ShoppingCartComponent},
+  {path:"user/login", component:LoginComponent},
   {path:'',redirectTo: "/",pathMatch:"full"},
-  {path:'**', component:ProductListComponent}
+  {path:'**', component:ProductListComponent},
 ];
 
 @NgModule({
