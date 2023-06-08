bf446cdbe7248f81bb0c4b16d8f1db99e964bc65
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-06-08 04:03:51 +0300

    hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@ export const environment = {
 
   BaseUrl:"https://fakestoreapi.com/",
 
-  firebaseConfig :{
-    apiKey: "AIzaSyDvb0l3GputUibidqePPzoZjNje2zWN4L4",
-    authDomain: "fir-firstproject-9df42.firebaseapp.com",
-    projectId: "fir-firstproject-9df42",
-    storageBucket: "fir-firstproject-9df42.appspot.com",
-    messagingSenderId: "380867372078",
-    appId: "1:380867372078:web:26048d0b9e23c4a58f6bbf",
-    measurementId: "G-QFZ9VXZY8M"
+  firebaseConfig : {
+    apiKey: "AIzaSyA0YPw_XqX-oQ8ajHJSyGMpA5bcYiuH20s",
+    authDomain: "ecommerce-4110c.firebaseapp.com",
+    projectId: "ecommerce-4110c",
+    storageBucket: "ecommerce-4110c.appspot.com",
+    messagingSenderId: "989872795631",
+    appId: "1:989872795631:web:d4a874c8763c42a23d501a",
+    measurementId: "G-EYY3KP8YTV"
   }
 
 };
